3540e35a0bbfcdddb6f8ef6c3cd1b2acbd972474
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-07-05 19:46:16 +0200

    Update Css and add JS methods

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-const pencilColor = document.getElementById('pencil-color');
-const backgroundColor = document.getElementById('background-color');
-const pencilSize = document.getElementById('pencil-size');
+const pencilColor = document.getElementById("pencil-color");
+const backgroundColor = document.getElementById("background-color");
+const pencilSize = document.getElementById("pencil-size");
 
-const clear = document.getElementById('clear');
-const save = document.getElementById('save');
+const clear = document.getElementById("clear");
+const save = document.getElementById("save");
+
+let pencilColorValue = pencilColor.value;
+let pencilSizeValue = pencilSize.value;
 
 function setup() {
-    createCanvas(1900, 850);
-    // saveCanvas('pieceOfArt', 'jpg');
+  createCanvas(2000, 1000);
 }
 
 function draw() {
-    if (mouseIsPressed === true) {
-        point(mouseX, mouseY);
-        strokeWeight(10);
-    } else {
-        null;
-    }
+  if (mouseIsPressed) {
+    stroke(pencilColorValue);
+    strokeWeight(pencilSizeValue);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
 }
+
+function changeColor() {
+  pencilColorValue = pencilColor.value;
+}
+
+function changeSize() {
+  pencilSizeValue = pencilSize.value;
+}
+
+console.log(pencilColorValue);
+
+pencilColor.addEventListener("input", changeColor);
+pencilSize.addEventListener("input", changeSize);
